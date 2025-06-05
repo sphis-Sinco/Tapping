282ec01f0d1175b92e0f5ca49dc20057df809beb
Sphis-Sinco <sinconsistencia@gmail.com>
2025-06-04 23:33:23 -0500

    sinlib manual import

---
+++ source/import.hx
@@ -0,0 +1,11 @@
+import flixel.*;
+import flixel.graphics.frames.FlxAtlasFrames;
+import haxe.*;
+import lime.utils.Assets;
+
+using StringTools;
+
+#if sys
+import sys.FileSystem;
+import sys.io.File;
+#end
